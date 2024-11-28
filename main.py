3c9bd61aa4a0cae7ec23e6b095685f4d3da33e50
Ryan Chen <ryanchen175@gmail.com>
2024-11-28 15:06:33 -0800

    Cleanup bot commands

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 import discord
+from discord.ext import commands
 from random_map import *
 from secrets_storage import *
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}!')
 
-@client.event
-async def on_message(message):
-    # Avoid anything by own bot
-    if message.author == client.user:
-        return
-    # Get a random map
-    if message.content.startswith('$mapr'):
-        print("[Finding Random Map]")
-        beatmap = find_random_map().beatmaps[-1].url
-        await message.channel.send(beatmap)
-        print("[Random Map Sent]")
-    # Get a trivia question
-client.run(DISCORD_KEY)
+@bot.command()
+async def mapr(ctx, arg=None):
+    print("[Finding Random Map]")
+    beatmap = find_random_map().beatmaps[-1].url
+    await ctx.send(beatmap)
+    print("[Random Map Sent]")
+    
+bot.run(DISCORD_KEY)
 
