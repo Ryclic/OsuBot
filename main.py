4bc354c503d997e3eef9e9069f8f0140f2cd2ffd
Ryan Chen <ryanchen175@gmail.com>
2024-11-30 00:26:38 -0800

    Add shared responses option

--- main.py
+++ main.py
@@ -19,35 +19,44 @@ async def on_ready():
     print(f"We have logged in as {bot.user}!")
 
 @bot.command()
-async def rmap(ctx, arg=None):
+async def rmap(ctx, difficulty=None):
     print("[Finding Random Map]")
     beatmap = find_random_map().beatmaps[-1].url
     await ctx.reply(beatmap)
     print("[Random Map Sent]")
     
 @bot.command()
-async def bgtrivia(ctx, arg=1):
+async def bgtrivia(ctx, difficulty=1, shared="no"):
     print("[Selecting Random Map]")
-    challenge_map = select_map(arg)
+    challenge_map = select_map(difficulty)
     challenge_image = challenge_map.covers.cover_2x
     # Get rid of (TV Size) headers, etc for better matching
     map_title = re.sub(r'\[[^\]]*\]|\([^)]*\)', '', challenge_map.title).lower().strip()
 
     embed = discord.Embed(title="What map is this background from?")
     embed.set_image(url=challenge_image)
-    embed.set_footer(text="Difficulty Level: " + str(arg) + ", Timeout: 30 seconds")
+    embed.set_footer(text="Difficulty Level: " + str(difficulty) + ", Timeout: 30 seconds")
 
     await ctx.reply(embed=embed)
     print("[Map Challenge Sent: " + map_title + "]")
 
     def check(m):
-        if m.channel == ctx.channel and m.author.id == ctx.author.id:
-            if m.content == "skip":
-                raise NotImplementedError()
-            elif fuzz.ratio(map_title, m.content) >= 50:
-                return True
-            else:
-                return False
+        if shared == "yes":
+            if m.channel == ctx.channel:
+                if m.content == "skip":
+                    raise NotImplementedError()
+                elif fuzz.ratio(map_title, m.content) >= 50:
+                    return True
+                else:
+                    return False
+        else:
+            if m.channel == ctx.channel and m.author.id == ctx.author.id:
+                if m.content == "skip":
+                    raise NotImplementedError()
+                elif fuzz.ratio(map_title, m.content) >= 50:
+                    return True
+                else:
+                    return False
             
     try:
         await bot.wait_for("message", timeout=30.0, check=check)
@@ -59,9 +68,9 @@ async def bgtrivia(ctx, arg=1):
         await ctx.reply("That's correct! The map was: **" + map_title + "** \n" + challenge_map.beatmaps[-1].url)
 
 @bot.command()
-async def strivia(ctx, arg=1):
+async def strivia(ctx, difficulty=1, shared="no"):
     print("[Selecting Random Map]")
-    challenge_map = select_map(arg)
+    challenge_map = select_map(difficulty)
     challenge_mp3_url = "https://" + challenge_map.preview_url[2:]
     mp3_data = None
     # Store the mp3 in memory
@@ -79,20 +88,29 @@ async def strivia(ctx, arg=1):
     map_title = re.sub(r'\[[^\]]*\]|\([^)]*\)', '', challenge_map.title).lower().strip()
 
     embed = discord.Embed(title="What map uses this song?")
-    embed.set_footer(text="Difficulty Level: " + str(arg) + ", Timeout: 30 seconds")
+    embed.set_footer(text="Difficulty Level: " + str(difficulty) + ", Timeout: 30 seconds")
 
     await ctx.reply(embed=embed)
     await ctx.reply(file=discord.File(mp3_data, filename="song.mp3"))
     print("[Map Challenge Sent: " + map_title + "]")
 
     def check(m):
-        if m.channel == ctx.channel and m.author.id == ctx.author.id:
-            if m.content == "skip":
-                raise NotImplementedError()
-            elif fuzz.ratio(map_title, m.content) >= 50:
-                return True
-            else:
-                return False
+        if shared == "yes":
+            if m.channel == ctx.channel:
+                if m.content == "skip":
+                    raise NotImplementedError()
+                elif fuzz.ratio(map_title, m.content) >= 50:
+                    return True
+                else:
+                    return False
+        else:
+            if m.channel == ctx.channel and m.author.id == ctx.author.id:
+                if m.content == "skip":
+                    raise NotImplementedError()
+                elif fuzz.ratio(map_title, m.content) >= 50:
+                    return True
+                else:
+                    return False
 
     try:
         await bot.wait_for("message", timeout=30.0, check=check)
